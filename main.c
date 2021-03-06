a4897bf332fdf7678452831c4c545d0381b90695
tomohiro <drs-tomo@hotmail.co.jp>
2021-03-06 12:14:25 +0900

    lesson 2021/2/27

--- main.c
+++ main.c
@@ -2,19 +2,30 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
+#include <omp.h>
 
 void CreateMatrix(int *matrix, int N) {
+//#pragma omp parallel for
     for (int i = 0; i < N * N; i++) {
         matrix[i] = i + 1;
     }
 }
 
 void CalculateMatrix(int *matrix1, int *matrix2, int *matrix3, int N) {
+    // ここでmatrix2は転置したとする
     for (int j = 0; j < N; ++j) {
         for (int i = 0; i < N; ++i) {
             int sum = 0;
+//            int in = i * N;
             for (int k = 0; k < N; ++k) {
-                sum += matrix1[i * N + k] * matrix2[k * N + j];
+                sum += matrix1[i * N + k + 0] * matrix2[(j + 0) * N + k];
+//                sum += matrix1[in + k + 1] * matrix2[(k + 1) * N + j];
+//                sum += matrix1[in + k + 2] * matrix2[(k + 2) * N + j];
+//                sum += matrix1[in + k + 3] * matrix2[(k + 3) * N + j];
+//                sum += matrix1[in + k + 4] * matrix2[(k + 4) * N + j];
+//                sum += matrix1[in + k + 5] * matrix2[(k + 5) * N + j];
+//                sum += matrix1[in + k + 6] * matrix2[(k + 6) * N + j];
+//                sum += matrix1[in + k + 7] * matrix2[(k + 7) * N + j];
             }
             matrix3[i * N + j] = sum;
         }
@@ -62,6 +73,17 @@ int main(int argc, char *argv[]) {
     printf("%ld.%06lu\n", etv.tv_sec - stv.tv_sec, etv.tv_usec - stv.tv_usec);
 
     MeasureCreateTime(CreateMatrix, matrix1, matrix2, matrix3, N);
+    int ans = 0;
+#pragma omp parallel for reduction(+:ans)
+    for (int i = 0; i < N; ++i) {
+//        printf("i = %d, thread = %d\n", i, omp_get_thread_num());
+//        ans += matrix1[i] * matrix2[i];
+        ans += 1 + 1;
+    }
+
+    printf("ans = %d\n", ans);
+    printf("threads = %d\n", omp_get_max_threads());
+
     MeasureCalculateTime(CalculateMatrix, matrix1, matrix2, matrix3, N);
 
     printf("main end\n");
--- main.sh
+++ main.sh
@@ -2,14 +2,17 @@
 
 #gcc main.c -o main && ./main 1000 && ./main 3000 && ./main 4000
 
-gcc main.c -o main &&
-./main 100 &&
-./main 200 &&
-./main 300 &&
-./main 400 &&
-./main 500 &&
-./main 600 &&
-./main 700 &&
-./main 800 &&
-./main 900 &&
-./main 1000
+#gcc main.c -o main &&
+#./main 100 &&
+#./main 200 &&
+#./main 300 &&
+#./main 400 &&
+#./main 500 &&
+#./main 600 &&
+#./main 700 &&
+#./main 800 &&
+#./main 900 &&
+#./main 1000
+
+#gcc main.c -O3 -march=native -mavx2 -o main && ./main 1000
+gcc main.c -O3 -o main && ./main 1000
